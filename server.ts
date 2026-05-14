2369294da88421a300b774779922ac37bae71ab9
szantodenis12 <denis.szanto1@gmail.com>
2026-05-14 16:25:14 +0300

    Fix port configuration for deployment

--- server.ts
+++ server.ts
@@ -23,7 +23,7 @@ const db = admin.firestore();
 
 async function startServer() {
   const app = express();
-  const PORT = 3000;
+  const PORT = process.env.PORT || 3000;
 
   app.use(express.json());
   app.use(cookieParser());
